98ff6604a0bfc576e7e48f6d975a1fa9722e68f3
Kacper Klys <kacper.klys@e9controls.com>
2023-10-23 09:59:57 +0000

    Add tests to verify delays

--- integration_test.go
+++ integration_test.go
@@ -12,7 +12,35 @@ import (
 	"github.com/e9ctrl/vd/stream"
 )
 
-const ADDR = "localhost:3333"
+const (
+	ADDR1 = "localhost:3333"
+	ADDR2 = "localhost:4444"
+)
+
+func setupTestCaseDelays(t *testing.T) func() {
+	//read file
+	vdfile, err := stream.ReadVDFile("stream/vdfile_delays")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//read file
+	d, err := stream.NewDevice(vdfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s, err := server.New(d, ADDR2)
+	if err != nil {
+		t.Fatalf("error while creating server %v\n", err)
+	}
+
+	s.Start()
+
+	return func() {
+		s.Stop()
+	}
+}
 
 func setupTestCase(t *testing.T) func() {
 	//read file
@@ -27,7 +55,7 @@ func setupTestCase(t *testing.T) func() {
 		t.Fatal(err)
 	}
 
-	s, err := server.New(d, ADDR)
+	s, err := server.New(d, ADDR1)
 	if err != nil {
 		t.Fatalf("error while creating server %v\n", err)
 	}
@@ -40,13 +68,13 @@ func setupTestCase(t *testing.T) func() {
 }
 
 func TestRun(t *testing.T) {
+	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 	defer setupTestCase(t)()
 	// connect to server
-	conn, err := net.Dial("tcp", ADDR)
-
+	conn, err := net.Dial("tcp", ADDR1)
 	if err != nil {
 		t.Fatalf("could not connect to to server: %v\n", err)
 	}
@@ -89,12 +117,13 @@ func TestRun(t *testing.T) {
 }
 
 func TestRunWrongQueries(t *testing.T) {
+	t.Parallel()
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
 	defer setupTestCase(t)()
 	// connect to server
-	conn, err := net.Dial("tcp", ADDR)
+	conn, err := net.Dial("tcp", ADDR1)
 
 	if err != nil {
 		t.Fatalf("could not connect to to server: %v\n", err)
@@ -130,6 +159,55 @@ func TestRunWrongQueries(t *testing.T) {
 			_, err := conn.Read(out)
 			if !errors.Is(err, os.ErrDeadlineExceeded) {
 				t.Errorf("exp error: %s got: %s\n", os.ErrDeadlineExceeded, err)
+			}
+		})
+	}
+}
+
+func TestRunWithDelays(t *testing.T) {
+	t.Parallel()
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	defer setupTestCaseDelays(t)()
+	// connect to server
+	conn, err := net.Dial("tcp", ADDR2)
+	if err != nil {
+		t.Fatalf("could not connect to to server: %v\n", err)
+	}
+	defer conn.Close()
+	tests := []struct {
+		name  string
+		dur   time.Duration
+		input []byte
+		want  []byte
+	}{
+		{"current check", 3 * time.Second, []byte("CUR?\r\n"), []byte("CUR 300\r\n")},
+		{"psi check", 4 * time.Second, []byte("PSI?\r\n"), []byte("PSI 3.30\r\n")},
+		{"temp check", 2 * time.Second, []byte("TEMP?\r\n"), []byte("TEMP 2.30\r\n")},
+		{"current set", 3 * time.Second, []byte("CUR 20\r\n"), []byte("OK\r\n")},
+		{"psi set", 4 * time.Second, []byte("PSI 3.46\r\n"), []byte("PSI 3.46 OK\r\n")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// set timeout for reading data
+			conn.SetReadDeadline(time.Now().Add(tt.dur))
+			if _, err := conn.Write(tt.input); err != nil {
+				t.Error("could not write payload to TCP server:", err)
+			}
+
+			out := make([]byte, 128)
+			start := time.Now()
+			_, err := conn.Read(out)
+			elapsed := time.Since(start)
+			if err == nil {
+				trimmed := bytes.Trim(out, "\x00")
+				if !bytes.Equal(tt.want, trimmed) {
+					t.Errorf("exp resp: %[1]v %[1]s got: %[2]v %[2]s\n", tt.want, trimmed)
+				}
+			}
+			if elapsed >= tt.dur && elapsed < tt.dur+5*time.Microsecond {
+				t.Errorf("exp delay around: %v got: %v\n", tt.dur, elapsed)
 			}
 		})
 	}
