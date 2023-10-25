7ace8e04fd8f1dd3a612cad335053d34ab7a88e4
Kacper Klys <kacper.klys@e9controls.com>
2023-10-25 21:50:10 +0000

    Add cli commands to control delays

---
+++ cmd/set_delay.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/e9ctrl/vd/api"
+
+	"github.com/spf13/cobra"
+)
+
+var setDelayCmd = &cobra.Command{
+	Use:   "delay [delay_type] [value]\n  vd set delay [delay_type] [parameter name] [value]",
+	Args:  cobra.RangeArgs(2, 3),
+	Short: "Command to set value of delays",
+	Long: `The command sets value of global and parameter delays of two types: reponse and acknowledge.
+	It communicates with REST API of the simulator and using HTTP POSRT verb modifies value of the specified delay. 
+Examples:
+	vd set delay res 10s		-> set global response delay
+	vd set delay ack 10s		-> set global acknowledge delay
+	vd set delay res temp 100ms	-> set response delay of temp parameter
+	vd set delay ack volt 1m	-> set acknowledge delay of volt parameter
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		addr, err := cmd.Flags().GetString("apiAddr")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if !verifyIPAddr(addr) {
+			fmt.Println("Wrong HTTP address")
+			os.Exit(1)
+		}
+
+		c := api.NewClient(addr)
+		var param string
+		if len(args) == 2 {
+			param = fmt.Sprintf("delay/%s", args[0])
+			err = c.SetParameter(param, args[1])
+		} else {
+			param = fmt.Sprintf("delay/%s/%s", args[0], args[1])
+			err = c.SetParameter(param, args[2])
+		}
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("OK")
+	},
+}
+
+func init() {
+	setCmd.AddCommand(setDelayCmd)
+}
