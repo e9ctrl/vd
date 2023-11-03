1f2cea832e8fe5c533891f4bfb8b3638ac533045
Kacper Klys <kacper.klys@e9controls.com>
2023-11-03 15:31:11 +0000

    Add custom client method to trigger parameter

--- api/client.go
+++ api/client.go
@@ -16,6 +16,26 @@ func NewClient(url string) *client {
 	}
 }
 
+func (c *client) TriggerParam(param string) error {
+	resp, err := http.Post("http://"+c.url+"/trigger/"+param, "text/plain", nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("API error %s", body)
+	}
+	return nil
+}
+
 func (c *client) GetParameter(param string) (string, error) {
 	resp, err := http.Get("http://" + c.url + "/" + param)
 	if err != nil {
