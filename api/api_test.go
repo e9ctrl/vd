0b3ddcba8bf9e8b5de51e41a34efe1c0562eb5b7
Kacper Klys <kacper.klys@e9controls.com>
2023-11-07 09:14:17 +0000

    Add more test cases in api pkg

--- api/api_test.go
+++ api/api_test.go
@@ -88,7 +88,6 @@ func TestSetMismatch(t *testing.T) {
 	}
 }
 
-// wrong parameter
 func TestGetParameter(t *testing.T) {
 	t.Parallel()
 	vdfile, err := stream.ReadVDFile(FILE1)
@@ -108,21 +107,32 @@ func TestGetParameter(t *testing.T) {
 	ts := newTestServer(t, a.routes())
 
 	defer ts.Close()
-	expected := `3.3`
-
-	code, _, body := ts.get(t, "/psi")
-	if code != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			code, http.StatusOK)
+	tests := []struct {
+		name    string
+		param   string
+		exp     string
+		expCode int
+	}{
+		{"get version", "version", "version 1.0", http.StatusOK},
+		{"get current", "current", "300", http.StatusOK},
+		{"get mode", "mode", "NORM", http.StatusOK},
+		{"get wrong paramter", "test", "Error: parameter test not found", http.StatusInternalServerError},
 	}
-	if string(body) != expected {
-		t.Errorf("handler returned unexpected body: got\n %s want\n %v",
-			body, expected)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, "/"+tt.param)
+			if code != tt.expCode {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					code, tt.expCode)
+			}
+			if string(body) != tt.exp {
+				t.Errorf("handler returned unexpected body: got\n %s want\n %v",
+					body, tt.exp)
+			}
+		})
 	}
 }
 
-// wrong parameter
-// wrong value
 func TestSetParameter(t *testing.T) {
 	t.Parallel()
 	vdfile, err := stream.ReadVDFile(FILE3)
@@ -142,27 +152,43 @@ func TestSetParameter(t *testing.T) {
 	ts := newTestServer(t, a.routes())
 
 	defer ts.Close()
-	expectedSet := `Parameter set successfully`
-	expectedGet := `4.56`
 
-	code, _, body := ts.set(t, "/psi/4.56")
-	if code != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			code, http.StatusOK)
+	tests := []struct {
+		name       string
+		param      string
+		set        string
+		expSet     string
+		expSetCode int
+		expGet     string
+		expGetCode int
+	}{
+		{"set psi", "psi", "4.56", "Parameter set successfully", http.StatusOK, "4.56", http.StatusOK},
+		{"set current", "current", "34", "Parameter set successfully", http.StatusOK, "34", http.StatusOK},
+		{"set wrong psi value", "psi", "5s", "Error: received param type that cannot be converted to float", http.StatusInternalServerError, "4.56", http.StatusOK},
+		{"set wrong parameter", "test", "20.1", "Error: parameter test not found", http.StatusInternalServerError, "Error: parameter test not found", http.StatusInternalServerError},
 	}
-	if string(body) != expectedSet {
-		t.Errorf("handler returned unexpected body: got\n %s want\n %v",
-			body, expectedSet)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.set(t, "/"+tt.param+"/"+tt.set)
+			if code != tt.expSetCode {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					code, tt.expSetCode)
+			}
+			if string(body) != tt.expSet {
+				t.Errorf("handler returned unexpected body: got\n %s want\n %v",
+					body, tt.expSet)
+			}
 
-	code, _, body = ts.get(t, "/psi")
-	if code != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			code, http.StatusOK)
-	}
-	if string(body) != expectedGet {
-		t.Errorf("handler returned unexpected body: got\n %s want\n %v",
-			body, expectedGet)
+			code, _, body = ts.get(t, "/"+tt.param)
+			if code != tt.expGetCode {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					code, tt.expGetCode)
+			}
+			if string(body) != tt.expGet {
+				t.Errorf("handler returned unexpected body: got\n %s want\n %v",
+					body, tt.expGet)
+			}
+		})
 	}
 }
 
@@ -235,15 +261,15 @@ func TestSetGlobDel(t *testing.T) {
 		name       string
 		typ        string
 		set        string
-		exp        string
-		expSetCode int
-		expGetCode int
 		expSet     string
+		expSetCode int
+		expGet     string
+		expGetCode int
 	}{
-		{"set global result delay", "res", "2s", "Delay set successfully", http.StatusOK, http.StatusOK, "2s"},
-		{"set global acknowledge delay", "ack", "3s", "Delay set successfully", http.StatusOK, http.StatusOK, "3s"},
-		{"set wrong delay type", "test", "5s", "Error: delay test not found", http.StatusInternalServerError, http.StatusInternalServerError, "Error: delay test not found"},
-		{"set wrong delay duration", "res", "test", "Error: time: invalid duration \"test\"", http.StatusInternalServerError, http.StatusOK, "2s"},
+		{"set global result delay", "res", "2s", "Delay set successfully", http.StatusOK, "2s", http.StatusOK},
+		{"set global acknowledge delay", "ack", "3s", "Delay set successfully", http.StatusOK, "3s", http.StatusOK},
+		{"set wrong delay type", "test", "5s", "Error: delay test not found", http.StatusInternalServerError, "Error: delay test not found", http.StatusInternalServerError},
+		{"set wrong delay duration", "res", "test", "Error: time: invalid duration \"test\"", http.StatusInternalServerError, "2s", http.StatusOK},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -252,9 +278,9 @@ func TestSetGlobDel(t *testing.T) {
 				t.Errorf("handler returned wrong status code: got %v want %v",
 					code, tt.expSetCode)
 			}
-			if string(body) != tt.exp {
+			if string(body) != tt.expSet {
 				t.Errorf("handler returned unexpected body: got\n %s want\n %v",
-					body, tt.exp)
+					body, tt.expSet)
 			}
 
 			code, _, body = ts.get(t, "/delay/"+tt.typ)
@@ -262,9 +288,9 @@ func TestSetGlobDel(t *testing.T) {
 				t.Errorf("handler returned wrong status code: got %v want %v",
 					code, tt.expGetCode)
 			}
-			if string(body) != tt.expSet {
+			if string(body) != tt.expGet {
 				t.Errorf("handler returned unexpected body: got\n %s want\n %v",
-					body, tt.expSet)
+					body, tt.expGet)
 			}
 		})
 	}
