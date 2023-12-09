f743121af10429dc11daa969b8297a4f0804358a
Kacper Klys <kacper.klys@e9controls.com>
2023-12-09 21:31:29 +0000

    Add more tests for parser pkg

--- protocols/stream/parser_test.go
+++ protocols/stream/parser_test.go
@@ -1,10 +1,225 @@
 package stream
 
 import (
+	"bytes"
+	"errors"
 	"testing"
 
 	"github.com/e9ctrl/vd/parameter"
+	"github.com/e9ctrl/vd/protocols"
+	"github.com/e9ctrl/vd/structs"
+	"github.com/e9ctrl/vd/vdfile"
 )
+
+func TestParse(t *testing.T) {
+	t.Parallel()
+	vd, err := vdfile.ReadVDFile("../../vdfile/vdfile")
+	if err != nil {
+		t.Fatalf("error while parsing test file: %v", err)
+	}
+	p, err := NewParser(vd)
+	if err != nil {
+		t.Fatalf("error while creating parser: %v", err)
+	}
+	tests := []struct {
+		name   string
+		in     string
+		exp    []byte
+		expCmd string
+		expErr error
+	}{
+		{"get command int", "CUR?", []byte("CUR 300"), "get_current", nil},
+		{"get command str", "VER?", []byte("version 1.0"), "get_version", nil},
+		{"set psi command", "PSI 30.42", []byte("PSI 30.42 OK"), "set_psi", nil},
+		{"empty command", "", []byte(""), "", protocols.ErrCommandNotFound},
+		{"non-existent command", "test 30.0", []byte(nil), "", protocols.ErrCommandNotFound},
+		{"wrong value of the command", "CUR 30.0", []byte(nil), "set_current", parameter.ErrWrongIntVal},
+		{"set command with opt", ":PULSE0:MODE SING", []byte("ok"), "set_mode", nil},
+		{"wrong opt of the command", ":PULSE0:MODE TEST", []byte(nil), "set_mode", parameter.ErrValNotAllowed},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, cmdName, err := p.Parse(tt.in)
+			if !errors.Is(err, tt.expErr) {
+				t.Errorf("exp error: %v got: %v", tt.expErr, err)
+			}
+			if !bytes.Equal(tt.exp, res) {
+				t.Errorf("exp response: %s got: %s", tt.exp, res)
+			}
+			if tt.expCmd != cmdName {
+				t.Errorf("exp cmd name: %s got: %s", tt.expCmd, cmdName)
+			}
+		})
+	}
+}
+
+func TestTrigger(t *testing.T) {
+	t.Parallel()
+	vd, err := vdfile.ReadVDFile("../../vdfile/vdfile")
+	if err != nil {
+		t.Fatalf("error while parsing test file: %v", err)
+	}
+	p, err := NewParser(vd)
+	if err != nil {
+		t.Fatalf("error while creating parser: %v", err)
+	}
+
+	tests := []struct {
+		name   string
+		cmd    string
+		exp    []byte
+		expErr error
+	}{
+		{"current param", "get_current", []byte("CUR 300"), nil},
+		{"version param", "get_version", []byte("version 1.0"), nil},
+		{"psi param", "get_psi", []byte("PSI 3.30"), nil},
+		{"empty command", "", []byte(nil), protocols.ErrCommandNotFound},
+		{"non-existent command", "test", []byte(nil), protocols.ErrCommandNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := p.Trigger(tt.cmd)
+			if !errors.Is(err, tt.expErr) {
+				t.Errorf("exp error: %v got: %v", tt.expErr, err)
+			}
+			if !bytes.Equal(tt.exp, res) {
+				t.Errorf("exp response: %s got: %s", tt.exp, res)
+			}
+		})
+	}
+}
+
+func TestBuildCommandPatterns(t *testing.T) {
+	input := map[string]*structs.Command{}
+	cmd1 := &structs.Command{
+		Name: "current_get",
+		Req:  []byte("get curr?"),
+		Res:  []byte("curr {%3.2f:current}"),
+	}
+	cmd2 := &structs.Command{
+		Name: "current_set",
+		Req:  []byte("set curr {%02d:current}"),
+		Res:  []byte("ok"),
+	}
+	cmd3 := &structs.Command{
+		Name: "version_get",
+		Req:  []byte("VER?"),
+		Res:  []byte("{%s:version}"),
+	}
+	cmd4 := &structs.Command{
+		Name: "psi_set",
+		Req:  []byte("set {%03X:psi} psi"),
+	}
+	input["current_get"] = cmd1
+	input["current_set"] = cmd2
+	input["version_get"] = cmd3
+	input["psi_set"] = cmd4
+
+	exp := map[string]CommandPattern{}
+
+	p1 := CommandPattern{
+		reqItems: []Item{Item{typ: ItemCommand, val: "get"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemCommand, val: "curr?"}},
+		resItems: []Item{Item{typ: ItemCommand, val: "curr"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemLeftMeta, val: "{"}, Item{typ: ItemNumberValuePlaceholder, val: "%3.2f"}, Item{typ: ItemParam, val: "current"}, Item{typ: ItemRightMeta, val: "}"}},
+	}
+	exp["current_get"] = p1
+
+	p2 := CommandPattern{
+		reqItems: []Item{Item{typ: ItemCommand, val: "set"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemCommand, val: "curr"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemLeftMeta, val: "{"}, Item{typ: ItemNumberValuePlaceholder, val: "%02d"}, Item{typ: ItemParam, val: "current"}, Item{typ: ItemRightMeta, val: "}"}},
+		resItems: []Item{Item{typ: ItemCommand, val: "ok"}},
+	}
+
+	exp["current_set"] = p2
+
+	p3 := CommandPattern{
+		reqItems: []Item{Item{typ: ItemCommand, val: "VER?"}},
+		resItems: []Item{Item{typ: ItemLeftMeta, val: "{"}, Item{typ: ItemStringValuePlaceholder, val: "%s"}, Item{typ: ItemParam, val: "version"}, Item{typ: ItemRightMeta, val: "}"}},
+	}
+
+	exp["version_get"] = p3
+
+	p4 := CommandPattern{
+		reqItems: []Item{Item{typ: ItemCommand, val: "set"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemLeftMeta, val: "{"}, Item{typ: ItemNumberValuePlaceholder, val: "%03X"}, Item{typ: ItemParam, val: "psi"}, Item{typ: ItemRightMeta, val: "}"}, Item{typ: ItemWhiteSpace, val: " "}, Item{typ: ItemCommand, val: "psi"}},
+	}
+	exp["psi_set"] = p4
+
+	cmdPattern, err := buildCommandPatterns(input)
+	if err != nil {
+		t.Fatalf("building pattern should not fail: %v", err)
+	}
+
+	for expKey, expVal := range exp {
+		val, exists := cmdPattern[expKey]
+
+		if !exists {
+			t.Errorf("expKey %s is not present", expKey)
+			return
+		}
+		for i, req := range val.reqItems {
+			if req.Type() != expVal.reqItems[i].Type() {
+				t.Errorf("param %s exp type %v on position %d got: %v", expKey, expVal.reqItems[i].Type(), i, req.Type())
+			}
+			if req.Value() != expVal.reqItems[i].Value() {
+				t.Errorf("param %s exp value %v on position %d got: %v", expKey, expVal.reqItems[i].Value(), i, req.Value())
+			}
+		}
+		for i, res := range val.resItems {
+			if res.Type() != expVal.resItems[i].Type() {
+				t.Errorf("param %s exp type %v on position %d got: %v", expKey, expVal.resItems[i].Type(), i, res.Type())
+			}
+			if res.Value() != expVal.resItems[i].Value() {
+				t.Errorf("param %s exp value %v on position %d got: %v", expKey, expVal.resItems[i].Value(), i, res.Value())
+			}
+		}
+	}
+}
+
+func TestBuildCommandPatternsEmptyCmds(t *testing.T) {
+	cmdPattern, err := buildCommandPatterns(nil)
+	if err != nil {
+		t.Fatalf("building pattern should not fail: %v", err)
+	}
+	if len(cmdPattern) != 0 {
+		t.Error("patterns should be empty")
+	}
+}
+
+func TestBuildCommandPatternsReqErr(t *testing.T) {
+	input := map[string]*structs.Command{}
+	cmd1 := &structs.Command{
+		Name: "current_get",
+		Req:  []byte("get curr{}?"),
+		Res:  []byte("curr {%3.2f:current}"),
+	}
+	input["current_get"] = cmd1
+
+	cmdPattern, err := buildCommandPatterns(input)
+	if cmdPattern != nil {
+		t.Error("patterns should be empty")
+	}
+	if !errors.Is(ErrWrongReqSyntax, err) {
+		t.Errorf("exp error: %v got %v", ErrWrongReqSyntax, err)
+	}
+}
+
+func TestBuildCommandPatternsResErr(t *testing.T) {
+	input := map[string]*structs.Command{}
+	cmd1 := &structs.Command{
+		Name: "current_get",
+		Req:  []byte("get curr?"),
+		Res:  []byte("curr {%3z.2f:current}"),
+	}
+	input["current_get"] = cmd1
+
+	cmdPattern, err := buildCommandPatterns(input)
+	if cmdPattern != nil {
+		t.Error("patterns should be empty")
+	}
+	if !errors.Is(ErrWrongResSyntax, err) {
+		t.Errorf("exp error: %v got %v", ErrWrongResSyntax, err)
+	}
+}
 
 func TestCheckPattern(t *testing.T) {
 	t.Parallel()
@@ -31,7 +246,6 @@ func TestCheckPattern(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			items := ItemsFromConfig(tt.forLex)
-			t.Logf("items: %v", items)
 
 			got, values := checkPattern(tt.input, items)
 			if got != tt.exp {
