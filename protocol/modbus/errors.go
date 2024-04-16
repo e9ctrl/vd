50934d8b1279622bc65530663061280e0be07b9c
Kacper Klys <kacper.klys@e9controls.com>
2024-04-16 06:38:11 +0000

    Remove not used error messages

--- protocol/modbus/errors.go
+++ protocol/modbus/errors.go
@@ -16,20 +16,6 @@ var (
 	IllegalDataAddress Exception = 2
 	// IllegalDataValue value is not accepted by slave.
 	IllegalDataValue Exception = 3
-	// SlaveDeviceFailure Unrecoverable error occurred while slave was attempting to perform requested action.
-	SlaveDeviceFailure Exception = 4
-	// AcknowledgeSlave has accepted request and is processing it, but a long duration of time is required. This response is returned to prevent a timeout error from occurring in the master. Master can next issue a Poll Program Complete message to determine whether processing is completed.
-	AcknowledgeSlave Exception = 5
-	// SlaveDeviceBusy is engaged in processing a long-duration command. Master should retry later.
-	SlaveDeviceBusy Exception = 6
-	// NegativeAcknowledge Slave cannot perform the programming functions. Master should request diagnostic or error information from slave.
-	NegativeAcknowledge Exception = 7
-	// MemoryParityError Slave detected a parity error in memory. Master can retry the request, but service may be required on the slave device.
-	MemoryParityError Exception = 8
-	// GatewayPathUnavailable Specialized for Modbus gateways. Indicates a misconfigured gateway.
-	GatewayPathUnavailable Exception = 10
-	// GatewayTargetDeviceFailedtoRespond Specialized for Modbus gateways. Sent when slave fails to respond.
-	GatewayTargetDeviceFailedtoRespond Exception = 11
 )
 
 func (e Exception) Error() string {
@@ -47,20 +33,6 @@ func (e Exception) String() string {
 		str = "IllegalDataAddress"
 	case IllegalDataValue:
 		str = "IllegalDataValue"
-	case SlaveDeviceFailure:
-		str = "SlaveDeviceFailure"
-	case AcknowledgeSlave:
-		str = "AcknowledgeSlave"
-	case SlaveDeviceBusy:
-		str = "SlaveDeviceBusy"
-	case NegativeAcknowledge:
-		str = "NegativeAcknowledge"
-	case MemoryParityError:
-		str = "MemoryParityError"
-	case GatewayPathUnavailable:
-		str = "GatewayPathUnavailable"
-	case GatewayTargetDeviceFailedtoRespond:
-		str = "GatewayTargetDeviceFailedtoRespond"
 	default:
 		str = "unknown"
 	}
