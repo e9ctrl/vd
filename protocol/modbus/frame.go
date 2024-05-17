c556c91a968faf5652e4e22de1fd62cec34911b9
Kacper Klys <kacper.klys@e9controls.com>
2024-05-17 09:17:03 +0000

    Add frame struct that represents single Modbus data packet

---
+++ protocol/modbus/frame.go
@@ -0,0 +1,125 @@
+package modbus
+
+import (
+	"encoding/binary"
+)
+
+// Constants with the Modbus function codes
+const (
+	ReadCoilsTyp             uint8 = 1
+	ReadDiscreteInputsTyp    uint8 = 2
+	ReadHoldingRegistersTyp  uint8 = 3
+	ReadInputRegistersTyp    uint8 = 4
+	WriteSingleCoilTyp       uint8 = 5
+	WriteHoldingRegisterTyp  uint8 = 6
+	WriteMultipleCoilsTyp    uint8 = 15
+	WriteHoldingRegistersTyp uint8 = 16
+)
+
+// Generate the Modbus function string name using its code
+func getFunctionName(f uint8) string {
+	switch f {
+	case ReadCoilsTyp:
+		return "ReadCoils"
+	case ReadDiscreteInputsTyp:
+		return "ReadDiscreteInputs"
+	case ReadHoldingRegistersTyp:
+		return "ReadHoldingRegisters"
+	case ReadInputRegistersTyp:
+		return "ReadInputRegisters"
+	case WriteSingleCoilTyp:
+		return "WriteSingleCoil"
+	case WriteHoldingRegisterTyp:
+		return "WriteHoldingRegister"
+	case WriteMultipleCoilsTyp:
+		return "WriteMultipleCoils"
+	case WriteHoldingRegistersTyp:
+		return "WriteHoldingRegisters"
+	default:
+		return "Unknown"
+	}
+}
+
+// TCPFrame is the Modbus TCP frame.
+type TCPFrame struct {
+	TransactionIdentifier uint16
+	ProtocolIdentifier    uint16
+	Length                uint16
+	Device                uint8
+	Function              uint8
+	Data                  []byte
+	Err                   *Exception
+}
+
+// NewTCPFrame converts a packet to a Modbus TCP frame.
+func NewTCPFrame(packet []byte) (*TCPFrame, error) {
+	// Check if the packet is too short.
+	if len(packet) < 9 {
+		return nil, ErrTCPPacketTooShort
+	}
+
+	frame := &TCPFrame{
+		TransactionIdentifier: binary.BigEndian.Uint16(packet[0:2]),
+		ProtocolIdentifier:    binary.BigEndian.Uint16(packet[2:4]),
+		Length:                binary.BigEndian.Uint16(packet[4:6]),
+		Device:                uint8(packet[6]),
+		Function:              uint8(packet[7]),
+		Data:                  packet[8:],
+		Err:                   &Success,
+	}
+
+	// Check expected vs actual packet length.
+	if int(frame.Length) != len(frame.Data)+2 {
+		return nil, ErrTCPLengthMismatch
+	}
+
+	return frame, nil
+}
+
+// Bytes returns the Modbus byte stream based on the TCPFrame fields.
+func (frame *TCPFrame) Bytes() []byte {
+	bytes := make([]byte, 8)
+
+	binary.BigEndian.PutUint16(bytes[0:2], frame.TransactionIdentifier)
+	binary.BigEndian.PutUint16(bytes[2:4], frame.ProtocolIdentifier)
+	binary.BigEndian.PutUint16(bytes[4:6], uint16(2+len(frame.Data)))
+	bytes[6] = frame.Device
+	bytes[7] = frame.Function
+	bytes = append(bytes, frame.Data...)
+
+	return bytes
+}
+
+// GetFunctionName returns the Modbus function name.
+func (frame *TCPFrame) GetFunctionName() string {
+	return getFunctionName(frame.Function)
+}
+
+// GetFunction returns the Modbus function code.
+func (frame *TCPFrame) GetFunction() uint8 {
+	return frame.Function
+}
+
+// GetData returns the TCPFrame Data byte field.
+func (frame *TCPFrame) GetData() []byte {
+	return frame.Data
+}
+
+// SetData sets the TCPFrame Data byte field and updates the frame length
+// accordingly.
+func (frame *TCPFrame) SetData(data []byte) {
+	frame.Data = data
+	frame.setLength()
+}
+
+// SetException sets the Modbus exception code in the frame.
+func (frame *TCPFrame) SetException() {
+	frame.Function = frame.Function | 0x80
+	frame.Data = []byte{byte(*frame.Err)}
+	frame.setLength()
+}
+
+// setLength sets length field according to data length.
+func (frame *TCPFrame) setLength() {
+	frame.Length = uint16(len(frame.Data))
+}
